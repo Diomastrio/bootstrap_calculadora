8d1f43fec400da7456b4793210c1faaa5730ed1f
andy contreras <zhchypyp@gmail.com>
2023-07-06 14:29:51 -0600

    calculadora funcionando

---
+++ src/App.js
@@ -0,0 +1,19 @@
+// import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+import ScientificCalculator from './ScientificCalculator';
+
+
+function App() {
+  return (
+    <div className="App">
+
+
+      <ScientificCalculator />
+
+
+    </div>
+  );
+}
+
+export default App;
